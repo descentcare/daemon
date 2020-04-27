881283ebce828208f595c150bac965b86ba4526a
Timofeev-Mikhail <student117@vm-73565ed9.netangels.ru>
2020-04-27 06:51:17 +0000

    Working already

--- simple.sh
+++ simple.sh
@@ -7,8 +7,8 @@ exec 0</dev/null
 trap 'echo "Config reloading..."' HUP
 trap 'echo "Finishing..."; exit' TERM
 
-while [[ 1 = 1 ]]; do
-	pwd > /home/student117/newfile
+while $(true); do
+	pwd >> /home/student117/newfile
 	sleep 10
 done
 
